d26cdb3bbdb9194c51cd920ede8b5641b7e6483b
Jose <jose.jessberger@suportis.com>
2022-09-21 16:30:11 +0200

    render bars dependend on data

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-console.log(d3);
-d3.select('#root')
-    .selectAll('p')
-    .data([1,2,3])
-    .enter()
-    .append('p')
-    .text(dta => `Zeile: ${dta}`);
+const DUMMY_DATA = [
+    { id: 'd1', value: 10, region: 'USA' },
+    { id: 'd2', value: 11, region: 'India' },
+    { id: 'd3', value: 12, region: 'China' },
+    { id: 'd4', value: 6, region: 'Germany' },
+  ];
+
+const container = d3.select('div')
+  .classed('container', true)
+  .style('border' , '1px solid red');
+
+const bars = container
+  .selectAll('.bar')
+  .data(DUMMY_DATA)
+  .enter()
+  .append('div')
+  .classed('bar', true)
+  .style('width', '50px')
+  .style('height',  data => ( data.value * 15 ) + 'px');
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 ﻿https://www.youtube.com/watch?v=TOJ9yjvlapY&list=RDCMUCSJbGtTlrDami-tDGPUV9-w&start_radio=1&rv=TOJ9yjvlapY&t=0
 
-
+https://github.com/QuantumSyncBot/D3Academin/commit/39acd47857da90ad1ffc039a63f1b7d34868ab7e
 ![image](https://user-images.githubusercontent.com/81805853/191521096-cc0060bb-9efc-4a36-a4b7-77bd127c419c.png)
