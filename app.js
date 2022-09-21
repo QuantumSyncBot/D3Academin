207e147142b0c7d969d14e5a88e00c4eb3afce03
Jose <jose.jessberger@suportis.com>
2022-09-21 17:08:15 +0200

    same bars in a svg

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ const DUMMY_DATA = [
     { id: 'd4', value: 6, region: 'Germany' },
   ];
 
-const container = d3.select('div')
-  .classed('container', true)
-  .style('border' , '1px solid red');
+const xScale    = d3.scaleBand().domain(DUMMY_DATA.map((dataPoint) => dataPoint.region))
+                    .rangeRound([0, 250])
+                    .padding(0.1);
+const yScale    = d3.scaleLinear().domain([0, 15]).range([200, 0]);
+
+const container = d3.select('svg')
+  .classed('container', true);
 
 const bars = container
   .selectAll('.bar')
   .data(DUMMY_DATA)
   .enter()
-  .append('div')
+  .append('rect')
   .classed('bar', true)
-  .style('border', '1px solid black')
-  .style('float', 'left')
-  .style('width', '50px')
-  .style('height',  data => ( data.value * 15 ) + 'px');
+  .attr('width', xScale.bandwidth())
+  .attr('height',  (data) => 200 - yScale(data.value) )
+  .attr('x', data => xScale(data.region))
+  .attr('y', data => yScale(data.value));
